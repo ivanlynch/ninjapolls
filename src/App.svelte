a3f3f9d9609da4a0b159d82ffc85096cef87c1ed
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-05-06 23:43:27 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import PollStore from "./stores/PollStore.js";
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import CreatePollForm from "./components/CreatePollForm.svelte";
+  import PollList from "./components/PollList.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  // tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+  const tabChange = e => {
+    activeItem = e.detail;
+  };
+
+  const handleAdd = e => {
+    activeItem = "Current Polls";
+  };
+
+</script>
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
+
+<Header />
+<main>
+  <Tabs {activeItem} {items} on:tabChange={tabChange} />
+  {#if activeItem === 'Current Polls'}
+    <PollList />
+  {:else if activeItem === 'Add New Poll'}
+    <CreatePollForm on:add={handleAdd} />
+  {/if}
+</main>
+<Footer />
